0140843e01135fccaf893a6848fcfd77812b6361
Avery Oliver <averyaoliver03@gmail.com>
2024-03-23 22:03:39 -0500

    problem 2 and 3

--- act2.rb
+++ act2.rb
@@ -5,3 +5,18 @@ def extract_hashtags(text)
 end
 
 p extract_hashtags("Love this #beautiful day! #sunny")
+
+def valid_password?(password)
+  pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$/
+  password.match?(pattern)
+end
+
+p valid_password?("Passw0rd")
+p valid_password?("pass")
+
+def find_numbers(text)
+  pattern = /\d+\.\d+|\d+/
+  text.scan(pattern)
+end
+
+p find_numbers("The order was placed for 100.25 units at a price of 20.75 each.")
