8e0f1c0e7cd94c7288d691383ca0d6b7e7e31e43
Avery Oliver <averyaoliver03@gmail.com>
2024-03-23 22:04:37 -0500

    problem 4 and 5 done

--- act2.rb
+++ act2.rb
@@ -20,3 +20,17 @@ def find_numbers(text)
 end
 
 p find_numbers("The order was placed for 100.25 units at a price of 20.75 each.")
+
+def valid_date?(date)
+  pattern = /\A\d{4}-\d{2}-\d{2}\z/
+  date.match?(pattern)
+end
+
+p valid_date?("2023-03-15")
+p valid_date?("15-03-2023")
+
+def parse_log_entry(log)
+  pattern = nil
+  match = log.match(pattern)
+  [match[1], match[2]] if match
+end
